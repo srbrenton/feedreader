027491f22839a4ad957f9670333c77cac0f1260a
Steve Brenton <sbrenton@pacbell.net>
2016-02-08 09:53:36 -0800

    intial entries testing complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -94,15 +94,22 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* This suite performs a rudimentary test of "Initial Entries" */
     describe('Initial Entries', function() {
 
-	/* TODO: Write a test that ensures when the loadFeed
+	/* This test that ensures when the loadFeed
 	 * function is called and completes its work, there is at least
 	 * a single .entry element within the .feed container.
-	 * Remember, loadFeed() is asynchronous so this test will require
-	 * the use of Jasmine's beforeEach and asynchronous done() function.
 	 */
+
+	beforeEach(function(done) {
+		loadFeed(0,done);
+	});
+
+	it('number at least one', function(done) {
+		expect($('.feed').children('.entry-link').children('.entry').length).toBeGreaterThan(0);
+		done();
+	});
 
     });
 
